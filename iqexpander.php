7d97139788fdf5026eea9e8c4ba754d2200868ae
Busse-Côté Lukas <pro.lukas.biscotte@gmail.com>
2025-04-03 15:20:32 +0200

    Site de type web

---
+++ iqexpander.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta name="robots" content="noindex">
+  <meta charset="UTF-8">
+  <title>IQ Expander Restored</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" href="assets/css/styleiqexpander.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+<body>
+
+  <header>
+  <img src="assets/images/IqExpander/TopHud.png" alt="Image en haut">
+  </header>
+
+  <main>
+    <img src="assets/images/IqExpander/Banner.png" alt="Logo de IQ Expander Restored" class="content-image">
+    <h2>
+      C'est quoi Iq Expander?
+    </h2>
+    <p>Iq Expander est un jeu video sorti en 1997 par Ravensburger Interactive. Il s'agit d'un jeu de puzzle de type point-and-click, principalement basé sur la mémoire.</p>
+    <p>Il comporte 15 mini-jeux différents, chacun avec 5 niveaux de difficulté, et jouables jusqu'à 4 joueurs.</p>
+
+    <h2>
+    Pourquoi l'avoir "restauré"
+    </h2>
+    <p>Malheureusement, ce jeu rencontre des problèmes de compatibilité avec les systèmes et environnements récents (au dela de Windows 95).</p>
+    <p>Ce jeu a marqué l'enfance de moi et mes freres, surtout par sa musique et son design. Un jour un de mes freres a evoqué l'idée de faire notre propre version du jeu avec 6 joueurs, c'est ce qui m'a poussé à me lancer dans la création d'une version restaurée de ce jeu.</p>
+
+
+    <h2>
+    Mon objectif
+    </h2>
+    <p>Rendre le jeu fonctionnel en y ajoutant des corrections et des améliorations de qualité de vie, tout en respectant au maximum la version originale.</p>
+    
+    <h2>
+    Comment je m'y suis pris?
+    </h2>
+    <p>
+    J'ai d'abord enregistré du gameplay du jeu dans une machine virtuelle à l'aide d'OBS, afin d'analyser précisément son fonctionnement d'origine.
+    </p>
+    <p>
+    J'ai ensuite extrait tous les sprites, musiques et effets sonores avec l'aide de <b>Adobe Director 11.5</b>, <b>Photoshop</b> et <b>FL Studio</b>.
+    </p>
+    <p>
+    Puis je me suis mis a coder sur <b>GameMaker Studio 2</b> en me fiant au gameplay enregistré plus tot et en m'aidant de divers sites et forums.
+    </p>
+
+
+    <h2>
+    Quels sont les améliorations?
+    </h2>
+
+    <li>Passage de 4 a 6 joueurs maximum</li>
+    <li>Informations sur quoi utiliser pour les jeux (souris et/ou clavier)</li>
+    <li>Ajout de personnages, d'effet sonores et musiques manquant au jeu original mais trouvé dans les fichiers</li>
+    <li>Ajout de fonctionnalités d'accessibilité, comme la possibilité de jouer au clavier et non uniquement à la souris</li>
+
+
+
+    <h2>
+    Galerie Photo
+    </h2>
+
+    <div class="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="assets/images/IqExpander/Screen01Size.png" alt="Image 1">
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/images/IqExpander/Screen02Size.png" alt="Image 2">
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/images/IqExpander/Screen03Size.png" alt="Image 3">
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/images/IqExpander/Screen04Size.png" alt="Image 4">
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/images/IqExpander/Screen05Size.png" alt="Image 5">
+      </div>
+    </div>
+
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    </div>
+
+
+
+
+
+    <h2>
+    Video montrant une petite partie solo
+    </h2>
+
+    <div class="video-container">
+      <iframe src="https://www.youtube.com/embed/vaio09V8-oY" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowfullscreen>
+      </iframe>
+    </div>
+    
+    <h2>
+    Où peut-on jouer a Iq Expander Restored?
+    </h2>
+      
+    <h3>
+    Le jeu est trouvable sur ma page <a href="https://mitainesmolles.itch.io/iq-expander-restored">itch.io!</a>
+    </h3>
+  </main>
+  <footer>
+    <div class="footer-container">
+      <img src="assets/images/IqExpander/BotHud.png" alt="Image en bas">
+      <a class="back-link" href="index.php">← Retour au portfolio</a>
+    </div>
+  </footer>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="assets/js/iqexpander.js"></script>
+</body>
+</html>
---
+++ ludoie.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta name="robots" content="noindex">
+  <meta charset="UTF-8">
+  <title>Ludoie</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/styleludoie.css">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+<body>
+
+  <main>
+    <img src="assets/images/Ludoie/MenuLogo.png" alt="Image du logo" class="content-image">
+
+    <h1>
+      Présentation du jeu et contexte
+    </h1>
+    <p>
+      Pendant ma licence, j'ai participé à un projet de groupe où nous devions créer un jeu vidéo avec d'autres étudiants de la formation, nous l'avons appelé Ludoie.
+    </p>
+    <p>
+      Ludoie combine les éléments classiques du jeu de société traditionnel Petits
+      Chevaux avec les mécaniques du jeu de l'oie. Les joueurs déplacent leurs pions sur un plateau
+      3D en suivant les règles familières du jeu. Avec des rebondissements imprévisibles, des choix
+      tactiques et la possibilité d'interagir avec les pions des autres joueurs.
+    </p>
+
+
+    <h1>
+      Équipe et organisation
+    </h1>
+
+    <p>
+      Nous étions une équipe de 12, chacun ayant ses rôles respectifs. Pour ma part, j'étais dans l'équipe d'IHM et de la communication du projet.
+    </p>
+
+    <p>
+      Nous organisions plusieurs réunions hebdomadaires tout au long du projet, avec que des versions alpha, bêta et finale, ainsi qu'une soutenance à la fin du projet accompagnée d'un rapport.
+    </p>
+
+
+    <h1>
+      Technologies et outils utilisés
+    </h1>
+
+    <p>
+      Pour le coté IHM je me suis servi de <b>Photoshop</b> principalement pour l'identité visuelle, de <b>FL Studio</b> pour l'identité sonore ainsi que de <b>Blender</b> pour la manipulation des pions et <b>Figma</b> pour créer les wireframes.
+    </p>
+
+    <p>
+      Pour ce qui est de la communication je me suis servi de <b>Photoshop</b> pour les montages photos et de <b>Adobe Premiere Pro</b> pour les montages vidéos.</p>
+
+    <h1>
+      Communication du Projet
+    </h1>
+
+    <button onclick="window.open('https://www.instagram.com/ludoiegame/', '_blank');">
+            Notre page Instagram
+    </button>
+    
+    <p>
+      Pour la communication autour du projet, nous avons réalisé plusieurs publications sur notre page Instagram. Voici celles dont je me suis personnellement occupé :
+    </p>
+    <h2>
+      Création de fausse boite du jeu:
+    </h2>
+
+    <div class="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="assets/images/Ludoie/LudoieCover.png" alt="Cover">
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/images/Ludoie/CoverFront.jpg" alt="Cover imprimée de face">
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/images/Ludoie/CoverBack.jpg" alt="Cover imprimée de dos">
+      </div>
+    </div>
+
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    </div>
+
+    <h2>
+      Création de bandes annonces pour le jeu:
+    </h2>
+
+    <button onclick="window.open('https://drive.google.com/file/d/1NAgkhjrVaFRqZjv4d3LbILpcVRQh9Y_c/view?usp=drive_link', '_blank');">
+            Trailer Alpha
+    </button>
+
+    <button onclick="window.open('https://drive.google.com/file/d/14wy0Hr0wgrw4mJ5p7ayfdF5pRz0OxKAS/view?usp=drive_link', '_blank');">
+            Trailer Final
+    </button>
+
+    <h2>
+      Création d'une fausse ligne de vêtements:
+    </h2>
+
+    <button onclick="window.open('https://www.instagram.com/ludoiegame/reel/C7EhJXvNON4/', '_blank');">
+            Ligne de vêtements
+    </button>
+
+    <h1>
+      Identité Visuelle et Sonore
+    </h1>
+
+    <p>
+      Voici ce dont je me suis occupé concernant l'identité visuelle et sonore du jeu:
+    </p>
+
+    <h2>
+      Bande son du jeu
+    </h2>
+
+    <p>
+    En ce qui concerne la musique j’ai décidé d’avoir une approche
+  plutôt dansante sur le menu principal avec de la House et un sample de musique disco des
+  années 80. J’ai parcouru d’autres styles de musique en ce qui
+  concerne les musiques de gameplay, la Drum and Bass et le Jazz.
+    </p>
+
+    <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1823197668&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/ludoiesoundtrack" title="ludoiesoundtrack" target="_blank" style="color: #cccccc; text-decoration: none;">ludoiesoundtrack</a> · <a href="https://soundcloud.com/ludoiesoundtrack/sets/ludoie-soundtrack" title="Ludoie Soundtrack" target="_blank" style="color: #cccccc; text-decoration: none;">Ludoie Soundtrack</a></div>
+    
+  </main>
+
+  <footer>
+    <button onclick="window.location.href='index.php';">
+     ← Retour au portfolio
+    </button>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="assets/js/iqexpander.js"></script>
+</body>
+</html>
