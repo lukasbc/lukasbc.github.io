b4f30edab2c2b6bf15ab809ce3b5650fd211030c
Busse-Côté Lukas <lukas.busse-cote@etu.unistra.fr>
2025-06-12 10:27:37 +0200

    Mise a jour de la page Ludoie

--- assets/js/ludoie.js
+++ assets/js/ludoie.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper', {
       loop: true,
@@ -12,4 +11,35 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     });
   });
-  
+
+function jouerSon(type) {
+      const son = document.getElementById(type);
+      if (son) {
+        son.currentTime = 0;
+        son.play();
+      }
+}
+
+function jouerSonFinished() {
+    const sons = [
+      document.getElementById("finish1"),
+      document.getElementById("finish2"),
+      document.getElementById("finish3"),
+      document.getElementById("finish4")
+    ];
+    const randomIndex = Math.floor(Math.random() * sons.length);
+    const son = sons[randomIndex];
+    son.currentTime = 0;
+    son.play();
+  }
+
+function jouerSonEat() {
+    const sons = [
+      document.getElementById("eat1"),
+      document.getElementById("eat2")
+    ];
+    const randomIndex = Math.floor(Math.random() * sons.length);
+    const son = sons[randomIndex];
+    son.currentTime = 0;
+    son.play();
+  }
